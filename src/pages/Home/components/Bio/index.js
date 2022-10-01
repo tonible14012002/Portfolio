22ca6748a1d73d7f5d9477285d0e47347808f67b
tonible14012002 <namanhble14012002@gmail.com>
2022-10-02 00:18:13 +0700

    fix UI

--- src/pages/Home/components/Bio/index.js
+++ src/pages/Home/components/Bio/index.js
@@ -1,49 +1,93 @@
 import Board from "../../../../components/Board"
 import Color from "../../../../components/Color"
 import Para from "../../../../components/Para"
+import Button from "../../../../components/Button"
+import URL from "../../../../components/URL"
 
 const Bio = () => {
     return (
         <div className=" rounded-lg p-2">
-            <div data-aos="fade-up" className="mb-3">
-                <h3 className="tablet:text-2xl text-lg">
-                    <Color primary>1. </Color> 
-                    <span>About me</span>
-                </h3>
-            </div>
-            <Board data-aos="fade-up" >
-                <h4 className="text-base">My name is <Color primary>Bui Ngoc Nam Anh. </Color></h4>
-                <h4 className="text-xs text-zinc-400 dark:text-zinc-700">( Fresher Software Engineer )</h4>
-                <Para blur className="mt-3 text-sm tablet:text-base">
-                    I am currently studying Computer Engineering bachelor at Ho Chi Minh University of Technology - one of the top tier universities based in Ho Chi Minh city, Viet Nam.
-                    I enjoy spending hours in cozy coffee shops for coding as well as studying new things. Especially, i have a strong passionate in algorithms and problem solving.
-                </Para>
-            </Board> 
-            <div data-aos="fade-up" className="mb-1 mt-10">
-                <h3 className="tablet:text-2xl text-lg">
-                    <Color primary>2.</Color> <span>Course Works</span>
-                </h3>
-            </div>
-            <div className="rounded-xl text-sm tablet:text-base text-zinc-200 pt-4 tablet:p-4 pb-4">
-                <div data-aos="fade-up" className="grid tablet:grid-cols-[100px_1fr] grid-cols-[90px_1fr] tablet:gap-5 gap-2">
-                    <span className="block font-semibold text-zinc-500">At University</span>
-                    <Para className="font-light ">
-                        <Color primary>Datastructure and Algorithms</Color>, Database System, 
-                        <Color primary>Computer Networking</Color>,
-                        <Color primary> Operating System</Color>.
-                    </Para>
+            <div data-aos="fade-up">
+                <div className="mb-3">
+                    <h3 className="tablet:text-2xl text-lg">
+                        <Color primary>1. </Color> 
+                        <span>About me</span>
+                    </h3>
                 </div>
-                <div data-aos="fade-up" className="grid grid-cols-[100px_1fr] tablet:grid-cols-[110px_1fr] gap-2 tablet:gap-5 mt-3 ">
-                    <span className="block font-semibold text-zinc-500">Online Courses</span>
-                    <Para className="font-light ">
-                        <Color primary>ReactJs fullcourse - Evondev</Color>, 
-                        F8 - elearning platform, 
-                        <Color primary>Mastering Datastructure and Algorithms - Abdul bari - Udemy</Color>,
-                        python - Socratica (youtube),
-                        Django - Traversy Media (youtube).
+                <Board >
+                    <h4 className="text-base">My name is <Color primary>Bui Ngoc Nam Anh. </Color></h4>
+                    <h4 className="text-xs text-zinc-400 dark:text-zinc-700">( Fresher Software Engineer )</h4>
+                    <Para blur className="mt-3 text-sm tablet:text-base">
+                        I am currently studying Computer Engineering bachelor at Ho Chi Minh University of Technology - one of the top tier universities based in Ho Chi Minh city, Viet Nam.
+                        I enjoy spending hours in cozy coffee shops for coding as well as studying new things. Especially, i have a strong passionate in algorithms and problem solving.
                     </Para>
+                </Board>           
+            </div>
+            <div data-aos="fade-up" className=" mt-6">
+                <div className="mb-1">
+                    <h3 className="tablet:text-2xl text-lg">
+                        <Color primary>2.</Color> <span>Course Works</span>
+                    </h3>
                 </div>
-            </div> 
+                <div className="rounded-xl text-sm tablet:text-base text-zinc-200 pt-4 tablet:p-4">
+                    <div className="grid tablet:grid-cols-[100px_1fr] grid-cols-[90px_1fr] tablet:gap-5 gap-2">
+                        <span className="block font-semibold text-zinc-500">At University</span>
+                        <Para className="font-light ">
+                            <Color primary>Datastructure and Algorithms</Color>, Database System, 
+                            <Color primary>Computer Networking</Color>,
+                            <Color primary> Operating System</Color>.
+                        </Para>
+                    </div>
+                    <div className="grid grid-cols-[100px_1fr] tablet:grid-cols-[110px_1fr] gap-2 tablet:gap-5 mt-3 ">
+                        <span className="block font-semibold text-zinc-500">Online Courses</span>
+                        <Para className="font-light ">
+                            <Color primary>ReactJs fullcourse - Evondev</Color>, 
+                            F8 - elearning platform, 
+                            <Color primary>Mastering Datastructure and Algorithms - Abdul bari - Udemy</Color>,
+                            python - Socratica (youtube),
+                            Django - Traversy Media (youtube).
+                        </Para>
+                    </div>
+                </div> 
+            </div>
+            <div data-aos="fade-up">
+                <div className="mb-1">
+                    <h3 className="tablet:text-2xl text-lg">
+                        <Color primary>3.</Color> <span>Contact</span>
+                    </h3>
+                </div>
+                <div  className="p-4 m-auto tablet:text-base text-sm">
+                    <div className="m-auto flex gap-4 items-baseline mb-2">
+                        <Color bg className="p-0 pl-2 pr-2">Gmail</Color>
+                        <span className="font-light break-all"
+                            href="https://www.instagram.com/14maroon/">
+                            <URL>
+                                namanhble14012002@gmail.com
+                            </URL>
+                        </span>
+                    </div>
+                    <div className="m-auto flex gap-4 items-baseline mb-2">
+                        <Color primary bg className="p-0 pl-2 pr-2">Facebook</Color>
+                        <Button className="text-rose-400 dark:text-pink-500 font-light break-all"
+                            href="https://www.facebook.com/namanh.bui.549/">
+                            <URL>
+                                https://www.facebook.com/namanh.bui.549/
+                            </URL>
+                        </Button>
+                    </div>
+                    <div className="m-auto flex gap-4 items-baseline mb-2">
+                        <Color primary bg className="p-0 pl-2 pr-2">Instagram</Color>
+                        <Button className="font-light break-all"
+                            href="https://www.instagram.com/14maroon/">
+                            <URL>
+                            https://www.instagram.com/14maroon/
+                            </URL>
+                        </Button>
+                    
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,52 +2,70 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
 import Button from "../../components/Button"
 import Bio from "./components/Bio"
-import { faFacebookF, faGithubAlt, faInstagram } from "@fortawesome/free-brands-svg-icons"
 import Color from "../../components/Color"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    const avatarSrc = ["/avatar.jpg", "/avatar1.jpg", "/avatar2.jpg"]
+    const [avatar, setAvatar] = useState(0)
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setAvatar(prev => {
+                console.log("set")
+                if (prev === avatarSrc.length-1) 
+                    return 0
+                else
+                    return prev + 1
+            })
+        }, 5000)
+        return () => clearInterval(timerId)
+    }, [avatarSrc.length])
 
     return (
         <div className="Home pt-4">
-            <div data-aos="fade-right" className="">
-                <h3 className="tablet:text-3xl text-2xl tablet:mb-3 mb-2">Hello!  <Color primary>Im  Nam Anh</Color></h3>
-                <h3 className="tablet:text-xl text-lg">2nd year Computer Engineer student</h3>
-                <h3 className="tablet:text-xl text-lg tablet:mb-8 mb-4">of HCM University of Technology</h3>
+            <div className="tablet:grid tablet:grid-cols-[1fr_150px]">
+                <div data-aos="fade-right" className="">
+                    <h3 className="tablet:text-3xl text-2xl tablet:mb-3 mb-2">Hello!  <Color primary>Im  Nam Anh</Color></h3>
+                    <h3 className="tablet:text-xl text-lg">2nd year Computer Engineer student</h3>
+                    <h3 className="tablet:text-xl text-lg tablet:mb-8 mb-4">of HCM University of Technology</h3>
 
-                <Button bg className="flex gap-1 hover:gap-2 transition-all w-[100px]">
-                    <span>My CV</span>
-                    <span> <FontAwesomeIcon icon={faChevronRight} /></span>
-                </Button>
-            </div>
-            <div data-aos="fade-left" className="w-full flex justify-end tablet:mt-4 mt-10">
-                <div className="tablet:w-[260px] tablet:h-[260px] 
-                        rounded-full -translate-y-4 overflow-hidden mr-4 outline outline-2 
-                        w-[180px] h-[180px]
-                        ">
+                    <Button bg className="flex gap-1 hover:gap-2 transition-all w-[100px]">
+                        <span>My CV</span>
+                        <span> <FontAwesomeIcon icon={faChevronRight} /></span>
+                    </Button>
+                </div>
+                <div data-aos="fade-left" className="rounded-full overflow-hidden outline outline-2
+                        m-auto
+                        mt-4
+                        w-[150px] h-[150px]"
+                >
                     <img
                         className="w-full h-full object-cover"
-                        alt="avatar" src="/avatar.jpg"
+                        alt="avatar" src={avatarSrc[avatar]}
                     />
                 </div>
             </div>
-            <div data-aos="fade-up" className="mt-10">
+            <hr className="mt-6 ml-4 mr-4 border-zinc-700 dark:border-zinc-400 m-auto"/>
+            {/* <div data-aos="fade-up" className="mt-0">
                 <ul className="flex justify-center gap-6 m-auto w-fit">
-                    <Button className="flex items-center justify-center text-lg hover:-translate-y-1 transition-all rotate-3 outline outline-2 tablet:outline-4  rounded-lg w-[30px] h-[30px] tablet:w-[40px] tablet:h-[40px]"
+                    <Button className="flex items-center justify-center text-lg hover:-translate-y-1 transition-all outline outline-2 tablet:outline-4  rounded-lg w-[30px] h-[30px] tablet:w-[40px] tablet:h-[40px]"
                         href="https://www.facebook.com/namanh.bui.549/"
                     >
                         <FontAwesomeIcon icon={faFacebookF} />
                     </Button>
-                    <Button className="flex items-center justify-center text-lg hover:-translate-y-1 transition-all -rotate-3 outline outline-2 tablet:outline-4  rounded-lg w-[30px] h-[30px] tablet:w-[40px] tablet:h-[40px]"
+                    <Button className="flex items-center justify-center text-lg hover:-translate-y-1 transition-all outline outline-2 tablet:outline-4  rounded-lg w-[30px] h-[30px] tablet:w-[40px] tablet:h-[40px]"
                         href="https://www.instagram.com/14maroon/"
                     >
                         <FontAwesomeIcon icon={faInstagram} />
                     </Button>
-                    <Button className="flex items-center justify-center text-lg hover:-translate-y-1 transition-all rotate-3 outline-2 tablet:outline-4   outline rounded-lg w-[30px] h-[30px] tablet:w-[40px] tablet:h-[40px]">
+                    <Button className="flex items-center justify-center text-lg hover:-translate-y-1 transition-all outline-2 tablet:outline-4   outline rounded-lg w-[30px] h-[30px] tablet:w-[40px] tablet:h-[40px]">
                         <FontAwesomeIcon icon={faGithubAlt} />
                     </Button>
                 </ul>
-            </div>
-            <div className="w-full mt-10">
+            </div> */}
+
+            <div className="w-full mt-2">
                 <Bio />
             </div>
             <div data-aos="fade-up"  className="w-fit m-auto">
